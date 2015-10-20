e1d2933d54386d2d2b61761d4faede2a8fedd62f
ggirdzis <ggirdzis@uvm.edu>
2015-10-20 18:48:13 -0400

    first

---
+++ sitemap.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Gretchen Girdzis CS148</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <h1>Gretchen Girdzis CS148</h1>
+    
+   
+    <figure class="me">
+        <img alt="An image of Gretchen Girdzis." src="assignment1.0/images/me.jpg">
+            
+    </figure>
+    <a href="tryme.php">Try Me!</a>
+    <a href="assignment1.0/index.php">Assignment1.0</a>
+    <a href="assignment2.0/select.php">Assignment2.0</a>
+    <a href="assignment3.0/join.php">Assignment3.0</a>
+    <a href="misc/friday.php">Friday Lab - October 2</a>
+    <a href="misc/friday.php">Friday Lab - October 9</a>
+    <a href="advising/">Advising</a>
+  
+    
+</html>
